4133ffe346ba790c485187b7df2cb603c02343f0
biyified <biyified@yahoo.com>
2010-11-24 18:59:17 +0100

    inner scrollfixed

--- html/js/action.js
+++ html/js/action.js
@@ -13,6 +13,27 @@ $(document).ready(function()
 																   {
 																	   $( this ).children('ul').slideUp(500);
 																	  });
-								
 							   
-							   });
+							   
+							   
+							    var myWidth = 0, myHeight = 0;
+							  if( typeof( window.innerWidth ) == 'number' ) {
+								//Non-IE
+								myWidth = window.innerWidth;
+								myHeight = window.innerHeight;
+							  } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+								//IE 6+ in 'standards compliant mode'
+								myWidth = document.documentElement.clientWidth;
+								myHeight = document.documentElement.clientHeight;
+							  } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+								//IE 4 compatible
+								myWidth = document.body.clientWidth;
+								myHeight = document.body.clientHeight;
+							  }
+							  
+							 $( "#contentArea").css('height',myHeight);
+							  $( "#contentArea").css('max-height',myHeight);
+							  $("#contentArea").css('overflow', "scroll");
+							 $('#formPanel').css('height',myHeight - 300 );
+ 
+});
